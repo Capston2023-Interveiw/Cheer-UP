3159c6d61bd4da5bbc0a645a3081a6634b94153e
hamin <hm5172@naver.com>
2023-04-06 17:44:19 +0900

    feat:#1 navbar 구현

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './Components/Header';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Header/>
+      
+  );
+}
+
+export default App;
