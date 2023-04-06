8ee7d675349daf6bfa9ee22fd5fde0cc6cfd02c2
son <wnsals2111@naver.com>
2023-04-06 17:41:39 +0900

    feat/#14: 로그인 페이지 input 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from "./pages/Login"
+import SignUp from "./pages/SignUp"
+
 
 function App() {
   return (
     <div className = "App">
-      <Login />
+      <SignUp />
     </div>
   );
 }
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -5,11 +5,11 @@ function LoginForm() {
   return (
     <form>
       <h1>Cheer Up</h1>
-      <i>
+      <lci>
         <input type="text" placeholder="아이디를 입력해주세요" />
         <input type="password" placeholder="비밀번호를 입력해주세요" />
         <button type="submit">로그인</button>
-      </i>
+      </lci>
       <p>
         <a href="#">비밀번호 찾기</a>
         <span> | </span>
