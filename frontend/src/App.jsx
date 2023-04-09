53e60d1f0185a8eccd804f0623668f350f225249
hamin <hm5172@naver.com>
2023-04-09 18:39:02 +0900

    feat: #18 버튼, 면접페이지 구현 중

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css';
-import Header from './Components/Header';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Welcome from './Pages/Welcome';
+import Interview from './Pages/Interview';
+
 
 function App() {
   return (
-    <Header/>
-      
+    <div className='App'>
+      <Router>
+        <Routes>
+          {/* ---- 메인페이지 ---- */}
+          <Route path='/' element={<Welcome />} />
+          {/* ---- 면접페이지 ---- */}
+          <Route path='/Interview' element={<Interview />} />
+          
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
