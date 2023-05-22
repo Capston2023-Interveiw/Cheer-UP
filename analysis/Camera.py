e99e9582e052eda8aba7b066e8d05d31b98374b5
김연진 <love_homebody@naver.com>
2023-05-22 20:36:02 +0900

    Refactor: #39 코드 정리

--- analysis/Camera.py
+++ analysis/Camera.py
@@ -2,229 +2,34 @@ import time
 import cv2
 import imutils
 import platform
-import wave
 import numpy as np
 from threading import Thread
 from queue import Queue
-
-from model.gaze.gaze_tracking.gaze_tracking import GazeTracking
-from model.gaze.gaze import run_gaze
-from model.posture.PoseProject import Posture
-from model.face.real_time_video import run_face
-from model.language.language import SttService
-import mediapipe as mp
-import pyaudio
-
-class DectectionModel:
-    def __init__(self):
-        self.mp_holistic = mp.solutions.holistic
-        self.holistic = self.mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5)
-        self.mp_drawing = mp.solutions.drawing_utils
-        self.postureClass = Posture(self.holistic)
-        self.gazeTracking = GazeTracking()
-        self.gazeFeedback = []
-        self.postureFeedback = []
-        self.expressionFeedback = []
-        self.current_time = time.time()
-        self.preview_time = time.time()
-        self.gazeCount = 0
-        self.shoulderCount = 0
-        self.postureCount = 0
-        self.expressionCount = 0
-        self.face = None
-        self.gaze = None
-        self.shoulder = None
-        self.head = None
-        self.x_left = 0
-        self.y_left = 0
-        self.x_right = 0
-        self.y_right = 0
-        self.fX = 0
-        self.fY = 0
-        self.fW = 0
-        self.fH = 0
-        self.faceColor = (2, 247, 234)
-        self.gazeColor = (2, 247, 234)
-        self.shoulderColor = (2, 247, 234)
-        self.headColor = (2, 247, 234)
-        
-    def detection(self, frame):
-        self.current_time = time.time()
-        fps = 1 / (self.current_time - self.preview_time)
-        self.preview_time = self.current_time
-        fps += 1
-        posture = self.postureClass.run_posture(frame)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faceResult = run_face(frame)
-
-        if faceResult is not None:
-            self.face, self.fX, self.fY, self.fW, self.fH = faceResult
-        self.gaze, self.x_left, self.y_left, self.x_right, self.y_right = run_gaze(frame, self.gazeTracking)
-
-        if self.gaze == "left" or self.gaze == "right":
-            self.gazeCount += 1
-            self.gazeColor = (248, 72, 216)
-            if self.gazeCount >= 3 * fps:
-                self.gazeFeedback.append(self.gaze)
-                self.gazeCount = 0
-                self.gazeColor = (0, 0, 255)
-        else:
-            self.gazeColor = (2, 247, 234)
-
-        if posture != None:
-            self.head = posture[0]
-            self.shoulder = posture[1]
-            if posture[0] == 'Bad':
-                self.postureCount += 1
-                self.headColor = (248, 72, 216)
-                if self.postureCount >= 5 * fps:
-                    self.postureFeedback.append(posture[0])
-                    self.postureCount = 0
-                    self.headColor = (0, 0, 255)
-            else:
-                self.headColor = (2, 247, 234)
-            if posture[1]:
-                self.shoulderCount += 1
-                self.shoulderColor = (248, 72, 216)
-                if self.shoulderCount >= 5 * fps:
-                    self.postureFeedback.append("어깨 비대칭")
-                    self.shoulderCount = 0
-                    self.shoulderColor = (0, 0, 255)
-            else:
-                self.shoulderColor = (2, 247, 234)
-
-        if self.face != None:
-            if self.face != "happy" and self.face != "neutral":
-                self.expressionCount += 1
-                self.faceColor = (248, 72, 216)
-                if self.expressionCount >= 5 * fps:
-                    self.expressionFeedback.append(f"{self.face}한 표정")
-                    self.expressionCount = 0
-                    self.faceColor = (0, 0, 255)
-            else:
-                self.faceColor = (2, 247, 234)
-
-    def result(self):
-        gazeScore = 20
-        postureScore = 20
-        expressionScore = 20
-        if len(self.gazeFeedback) > 2:
-            gazeScore -= (len(self.gazeFeedback) - 2)
-        
-        if gazeScore < 0:
-            gazeScore = 0
-        
-        if len(self.postureFeedback) > 2:
-            postureScore -= (len(self.postureFeedback) - 2)
-
-        if postureScore < 0:
-            postureScore = 0
-        
-        if len(self.expressionFeedback) > 2:
-            expressionScore -= (len(self.expressionFeedback) - 2)
-
-        if expressionScore < 0:
-            expressionScore = 0
-
-        stt = SttService("./model/language/record.wav")
-        stt.getScripts()
-        while stt.isEnd == False:
-            interjectionResult = stt.getInterjectionResult()
-            speedResult = stt.getSpeedResult()
-        time.sleep(2)
-        content = {
-            "gaze": {
-                "field": "gaze",
-                "score": gazeScore,
-                "feedback": self.gazeFeedback
-            },
-            "posture": {
-                "field": "posture",
-                "score": postureScore,
-                "feedback": self.postureFeedback
-            },
-            "face": {
-                "field": "face",
-                "score": expressionScore,
-                "feedback": self.expressionFeedback
-            },
-            "interjection" : interjectionResult,
-            "speed" : speedResult
-        }
-        return content
-    
-class RecordThread:
-
-    def __init__(self, audiofile='./model/language/record.wav'):
-        self.thread = None
-        self.audio = pyaudio.PyAudio()
-        self.bRecord = True
-        self.audiofile = audiofile
-        self.chunk = 1024
-        self.format = pyaudio.paInt16
-        self.channels = 1
-        self.rate = 16000
-        self.wavstream = None
-        self.wavfile = None
-
-    def run(self):
-        self.wavstream = self.audio.open(format=self.format,
-                        channels=self.channels,
-                        rate=self.rate,
-                        input=True,
-                        frames_per_buffer=self.chunk)
-        self.wavfile = wave.open(self.audiofile, 'wb')
-        self.wavfile.setnchannels(self.channels)
-        self.wavfile.setsampwidth(self.audio.get_sample_size(self.format))
-        self.wavfile.setframerate(self.rate)
-        if self.thread is None:
-            self.thread = Thread(target=self.record, args=())
-            self.thread.daemon = False
-            self.thread.start()
-        self.bRecord = True
-
-    def record(self):
-        while True:
-            if self.bRecord:
-                self.wavfile.writeframes(self.wavstream.read(self.chunk))
-
-    def stoprecord(self):
-        self.bRecord = False
-        time.sleep(1)
-        self.wavstream.stop_stream()
-        self.wavstream.close()
-        self.audio.terminate()
-        if self.wavfile is not None:
-            self.wavfile.close()
-            self.wavfile = None
+from Detection import Detection
+from Record import Record
 
 class Camera:
     
     def __init__(self):
-        
         if cv2.ocl.haveOpenCL() :
             cv2.ocl.setUseOpenCL(True)
         self.capture = None
         self.width = 640
         self.height = 360
-        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
-        self.video = cv2.VideoWriter("test.avi", self.fourcc, cv2.CAP_PROP_FPS, (self.width, self.height))
         self.thread = None
         self.stat = False
-        self.videoWrite = False
         self.current_time = time.time()
         self.preview_time = time.time()
         self.sec = 0
         self.Q = Queue(maxsize=128)
         self.started = False
-        self.model = DectectionModel()
+        self.detection = Detection()
         self.soundRecord = None
 
-    def run(self, src = 0) :
-        
+    def run(self, src = 0):
         self.stop()
 
-        self.soundRecord = RecordThread()
+        self.soundRecord = Record()
         if platform.system() == 'Windows' :        
             self.capture = cv2.VideoCapture(src , cv2.CAP_DSHOW)
         
@@ -235,31 +40,38 @@ class Camera:
         self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.height)
 
         self.soundRecord.run()
+        self.threadStart()
+    
+    def threadStart(self):
         if self.thread is None:
             self.thread = Thread(target=self.update, args=())
             self.thread.daemon = False
             self.thread.start()
         self.started = True
-        self.videoWrite = True
-    
+
     def stop(self):
-        
         self.started = False
         if self.capture is not None:
             self.capture.release()
-            self.video.release()
-            self.soundRecord.bRecord = False
-            self.soundRecord.stoprecord()
-            self.videoWrite = False
+            self.stopRecording()
             self.clear()
-            
+        self.endThread()
+
+    def stopRecording(self):
+        self.soundRecord.bRecord = False
+        self.soundRecord.stopRecording()
+
+    def endThread(self):
+        if self.thread is not None:
+            self.thread.join()
+
     def update(self):
         while True:
             if self.started:
                 (grabbed, frame) = self.capture.read()
                 if grabbed:
+                    self.detection.detection(frame)
                     self.Q.put(frame)
-                    self.model.detection(frame)                    
            
     def clear(self):
         with self.Q.mutex:
@@ -269,43 +81,40 @@ class Camera:
         return self.Q.get()
 
     def blank(self):
-        
         return np.ones(shape=[self.height, self.width, 3], dtype=np.uint8)
     
     def bytescode(self):
-        
         if not self.capture.isOpened():
-            
             frame = self.blank()
-
         else :
-
             frame = imutils.resize(self.read(), width=int(self.width) )
-            cv2.putText(frame, "face: " + str(self.model.face), (30, 45), cv2.FONT_HERSHEY_DUPLEX, 0.7, (147, 58, 31), 1)
-            cv2.putText(frame, "head: " + str(self.model.head), (30, 75), cv2.FONT_HERSHEY_DUPLEX, 0.7, (147, 58, 31), 1)
-            cv2.putText(frame, "shoulder: " + str(self.model.shoulder), (250, 45), cv2.FONT_HERSHEY_DUPLEX, 0.7, (147, 58, 31), 1)
-            cv2.putText(frame, "gaze: " + str(self.model.gaze), (250, 75), cv2.FONT_HERSHEY_DUPLEX, 0.7, (147, 58, 31), 1)
-            cv2.line(frame, (self.model.x_left - 5, self.model.y_left), (self.model.x_left + 5, self.model.y_left), self.model.gazeColor)
-            cv2.line(frame, (self.model.x_left, self.model.y_left - 5), (self.model.x_left, self.model.y_left + 5), self.model.gazeColor)
-            cv2.line(frame, (self.model.x_right - 5, self.model.y_right), (self.model.x_right + 5, self.model.y_right), self.model.gazeColor)
-            cv2.line(frame, (self.model.x_right, self.model.y_right - 5), (self.model.x_right, self.model.y_right + 5), self.model.gazeColor)
-            cv2.rectangle(frame, (self.model.fX, self.model.fY), (self.model.fX + self.model.fW, self.model.fY + self.model.fH), self.model.faceColor, 2)
-            if self.model.postureClass.results is not None:
-                self.model.mp_drawing.draw_landmarks(frame, self.model.postureClass.results.pose_landmarks, {(12, 14), (11, 12), (11, 12)}, 
-                                        None,
-                                        self.model.mp_drawing.DrawingSpec(color=self.model.shoulderColor, thickness=2, circle_radius=4),
-                                        )
-               
-                self.model.mp_drawing.draw_landmarks(frame, self.model.postureClass.results.face_landmarks, self.model.mp_holistic.FACEMESH_CONTOURS, 
-                                            None,
-                                            self.model.mp_drawing.DrawingSpec(color=self.model.headColor, thickness=1, circle_radius=1)
-                                        )
-            self.video.write(frame)
+            self.visualize(frame)
             if self.stat:  
                 cv2.rectangle(frame, (0,0), (120,30), (0,0,0), -1)
                 fps = 'FPS : ' + str(self.fps())
         return cv2.imencode('.jpg', frame )[1].tobytes()
     
+    def visualize(self, frame):
+        cv2.putText(frame, "face: " + str(self.detection.face), (30, 45), cv2.FONT_HERSHEY_DUPLEX, 0.7, (147, 58, 31), 1)
+        cv2.putText(frame, "head: " + str(self.detection.head), (30, 75), cv2.FONT_HERSHEY_DUPLEX, 0.7, (147, 58, 31), 1)
+        cv2.putText(frame, "shoulder: " + str(self.detection.shoulder), (250, 45), cv2.FONT_HERSHEY_DUPLEX, 0.7, (147, 58, 31), 1)
+        cv2.putText(frame, "gaze: " + str(self.detection.gaze), (250, 75), cv2.FONT_HERSHEY_DUPLEX, 0.7, (147, 58, 31), 1)
+        cv2.line(frame, (self.detection.x_left - 5, self.detection.y_left), (self.detection.x_left + 5, self.detection.y_left), self.detection.gazeColor)
+        cv2.line(frame, (self.detection.x_left, self.detection.y_left - 5), (self.detection.x_left, self.detection.y_left + 5), self.detection.gazeColor)
+        cv2.line(frame, (self.detection.x_right - 5, self.detection.y_right), (self.detection.x_right + 5, self.detection.y_right), self.detection.gazeColor)
+        cv2.line(frame, (self.detection.x_right, self.detection.y_right - 5), (self.detection.x_right, self.detection.y_right + 5), self.detection.gazeColor)
+        cv2.rectangle(frame, (self.detection.fX, self.detection.fY), (self.detection.fX + self.detection.fW, self.detection.fY + self.detection.fH), self.detection.faceColor, 2)
+        if self.detection.postureClass.results is not None:
+            self.detection.mp_drawing.draw_landmarks(frame, self.detection.postureClass.results.pose_landmarks, {(12, 14), (11, 12), (11, 12)}, 
+                                    None,
+                                    self.detection.mp_drawing.DrawingSpec(color=self.detection.shoulderColor, thickness=2, circle_radius=4),
+                                    )
+            
+            self.detection.mp_drawing.draw_landmarks(frame, self.detection.postureClass.results.face_landmarks, self.detection.mp_holistic.FACEMESH_CONTOURS, 
+                                        None,
+                                        self.detection.mp_drawing.DrawingSpec(color=self.detection.headColor, thickness=1, circle_radius=1)
+                                    )
+            
     def fps(self):
         
         self.current_time = time.time()
--- analysis/app.py
+++ analysis/app.py
@@ -8,10 +8,10 @@ from Camera import Camera
 
 app = Flask( __name__ )
 app.config['JSON_AS_ASCII'] = False
-isProgress = False
 isStream = False
 result = None
 camera = Camera()
+
 @app.route('/progress')
 def home():
     return render_template('video.html')
@@ -19,9 +19,7 @@ def home():
 @app.route('/interview/progress')
 def stream():
     src = request.args.get('src', default = 0, type = int)
-    global isStream 
-    global result
-    result = None
+    global isStream
     isStream = True
     try:
         return Response(
@@ -46,36 +44,32 @@ def stream_gen(src):
 def end():
     global isStream
     isStream = False
-    result = camera.model.result()
+    result = camera.detection.result()
     time.sleep(5)
-    return render_template('result.html', data={"result": result})
+    return result
 
-cam = cv2.VideoCapture(0)  # use 0 for web camera
-cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
-def gen_frames():  # generate frame by frame from camera
-    while True:
-        # Capture frame-by-frame
-        success, frame = cam.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+# cam = cv2.VideoCapture(0)
+# cam.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+# cam.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-@app.route('/video_feed')
-def video_feed():
-    #Video streaming route. Put this in the src attribute of an img tag
-    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# def gen_frames(): 
+#     while True:
+#         success, frame = cam.read()
+#         if not success:
+#             break
+#         else:
+#             ret, buffer = cv2.imencode('.jpg', frame)
+#             frame = buffer.tobytes()
+#             yield (b'--frame\r\n'
+#                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
-
-@app.route('/')
-def index():
-    """Video streaming home page."""
-    return render_template('start.html')
+# @app.route('/')
+# def index():
+#     return render_template('start.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888, debug=True)
