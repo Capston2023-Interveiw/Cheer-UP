ec416c3a7a82283e91e94b56b20255fb0a8a57e3
김연진 <love_homebody@naver.com>
2023-06-06 00:34:21 +0900

    feat: #42 jwt 인증 설정

--- backend/src/main/java/com/example/backend/config/SwaggerConfig.java
+++ backend/src/main/java/com/example/backend/config/SwaggerConfig.java
@@ -7,9 +7,15 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
@@ -20,6 +26,8 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.OAS_30)
                 .useDefaultResponseMessages(true)
                 .apiInfo(apiInfo())
+                .securityContexts(List.of(this.securityContext()))
+                .securitySchemes(List.of(this.apiKey()))
                 .ignoredParameterTypes(AuthenticationPrincipal.class)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.example.backend.controller"))
@@ -33,5 +41,22 @@ public class SwaggerConfig {
                 .description("2023 종합설계 S1-5")
                 .version("0.1")
                 .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
     }
 }
--- backend/src/main/java/com/example/backend/controller/InterviewProgressController.java
+++ backend/src/main/java/com/example/backend/controller/InterviewProgressController.java
@@ -3,8 +3,6 @@ package com.example.backend.controller;
 import com.example.backend.dto.response.QuestionResponse;
 import com.example.backend.entity.Member;
 import com.example.backend.service.InterviewProgressService;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -21,7 +19,6 @@ public class InterviewProgressController {
 
     private final InterviewProgressService interviewProgressService;
 
-    @ApiImplicitParams({@ApiImplicitParam(name = "Authorization", value = "인증 토큰", required = true, dataType = "string", paramType = "header")})
     @GetMapping("/progress")
     public ResponseEntity<List<QuestionResponse>> getQuestionList(@AuthenticationPrincipal Member member) {
         return ResponseEntity.ok(interviewProgressService.getQuestionList(member.getId()));
