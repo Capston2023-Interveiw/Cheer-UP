91e4722ccfa649008377808c24d70ec3d8500d32
손형민 <wnsals2111@naver.com>
2023-06-16 18:29:50 +0900

    feat: #56 페이지 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
         <>
             <Router>
                 <Routes>
+                    
                     {/* ---- 분석 페이지 ---- */}
                     <Route path = '/' element={<Analysis/>}/>
                     {/* ---- 메인페이지_로그인 전 ---- */}
