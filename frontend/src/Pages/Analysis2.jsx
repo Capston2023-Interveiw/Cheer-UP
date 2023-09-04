bd97eb33250789da69f27854263e0ff13dc59edf
gudals123 <104682516+gudals123@users.noreply.github.com>
2023-09-04 15:32:19 +0900

    feat:#95  시각화 수정

--- frontend/src/Pages/Analysis2.jsx
+++ frontend/src/Pages/Analysis2.jsx
@@ -5,11 +5,7 @@ import Header_Aft from '../Components/Header_Aft';
 import PentagonGraph from '../Components/PentagonGraph';
 import axios from 'axios';
 import Analysis_NavBar1 from '../Components/Analysis_NavBar1';
-import Visualization_Speed from '../Components/Visualization_Speed';
-import Visualization_Face from '../Components/Visualization_Face';
-import Visualization_Gaze from '../Components/Visualization_Gaze';
-//import Visualization_Interjection from '../Components/Visualization_Interjection';
-import Visualization_Posture from '../Components/Visualization_Posture';
+
 
 const Form = styled.div`
     width: 100%;
@@ -29,10 +25,7 @@ const  ViewFrame = styled.div`
 const ViewFrame2 =styled.div`
     margin-top: 20px;
     margin-bottom: 20px;
-    width: 65%;
-    max-width: 1200px;
-    min-width: 1130px;
-    
+    width: 1000px;
     height: 1600px;
     border: 1px solid;
     position: relative;
@@ -74,8 +67,8 @@ const Video_Box  = styled.div`
     position: absolute;
     top: 7%;
     right: 5%;
-    width: 500px;
-    height: 350px;
+    width: 428px;
+    height: 300px;
     border: 1px solid;
 `;
 
@@ -135,7 +128,21 @@ const P2 =styled.p`
     font-size: 22px;
 `;
 
-const Face_Box = styled.button`
+const Summary_Box = styled.button`
+    position: absolute;
+    top: 55%;
+    left: 5%;
+    width: 400px;
+    height: 250px;
+    border: 1px solid;
+    border-radius: 20px;
+    background-color:#FFFF;
+    cursor: pointer;
+`;
+
+
+
+const Interjection_Box = styled.button`
     position: absolute;
     top: 32%;
     right: 5%;
@@ -147,53 +154,7 @@ const Face_Box = styled.button`
     cursor: pointer;
 `;
 
-const Gaze_Box = styled.button`
-    position: absolute;
-    top: 55%;
-    right: 5%;
-    width: 400px;
-    height: 250px;
-    border: 1px solid;
-    border-radius: 20px;
-    background-color:#FFFF;
-    cursor: pointer;
-`;
 
-const Interjection_Box = styled.button`
-    position: absolute;
-    top: 55%;
-    left: 5%;
-    width: 400px;
-    height: 250px;
-    border: 1px solid;
-    border-radius: 20px;
-    background-color:#FFFF;
-    cursor: pointer;
-`;
-
-const Posture_Box = styled.button`
-    position: absolute;
-    top: 78%;
-    left: 5%;
-    width: 400px;
-    height: 250px;
-    border: 1px solid;
-    border-radius: 20px;
-    background-color:#FFFF;
-    cursor: pointer;
-`;
-
-const Speed_Box = styled.button`
-    position: absolute;
-    top: 78%;
-    right: 5%;
-    width: 400px;
-    height: 250px;
-    border: 1px solid;
-    border-radius: 20px;
-    background-color:#FFFF;
-    cursor: pointer;
-`;
 
 
 
@@ -204,7 +165,7 @@ export default function Analysis2(){
     const [faceSummary, setFaceSummary] = useState('');
     const [postureSummary, setPostureSummary] = useState('');
     const [gazeSummary, setGazeSummary] = useState('');
-    //const [interjectionSummary, setInterjectionSummary] = useState('');
+    const [interjectionSummary, setInterjectionSummary] = useState('');
     const [speedSummary, setSpeedSummary] = useState('');
 
     //const num = props.video_num;
@@ -227,11 +188,19 @@ export default function Analysis2(){
             console.log(gaze_summary);
             console.log(interjection_summary);
             console.log(speed_summary);
+
+
             setFaceSummary(face_summary);
             setPostureSummary(posture_summary);
             setGazeSummary(gaze_summary);
-            //setInterjectionSummary(interjection_summary);
+            setInterjectionSummary(interjection_summary);
             setSpeedSummary(speed_summary);
+
+            console.log(faceSummary);
+            console.log(postureSummary);
+            console.log(gazeSummary);
+            console.log(interjectionSummary);
+            console.log(speedSummary);
 
             const total = data.find(item => item.analysis_type === 'total');
             const others = data.filter(item => item.analysis_type !== 'total');
@@ -278,35 +247,31 @@ export default function Analysis2(){
                         </Score_Box>
                     </Graph_Box>
                     <Video_Box>
-                        <video height="350px" width="500px" src={totalInfo.url} controls/>
+                        <video height="300px" width="428px" src={totalInfo.url} controls/>
                     </Video_Box>
-                    <Link to = '/Analysis_face'>
-                        <Face_Box>
-                            <Visualization_Face inputData={faceSummary}/>
-                        </Face_Box>
-                    </Link>
-                    <Link to = '/Analysis_gaze'>
-                        <Gaze_Box>
-                            <Visualization_Gaze inputData={gazeSummary}/>
-                        </Gaze_Box>
-                    </Link>
-                    <Link to = '/Analysis_interjection'>
-                        <Interjection_Box>
-                            워드 클라우드
-                        </Interjection_Box>
-                    </Link>
-                    <Link to = '/Analysis_posture'>
-                        <Posture_Box>
-                            <Visualization_Posture inputData={postureSummary}/>
-                        </Posture_Box>
-                    </Link>
-                    <Link to = '/Analysis_speed'>
-                        <Speed_Box>
-                            <Visualization_Speed inputData={speedSummary}/>
-                        </Speed_Box>
-                    </Link>
 
-                    
+                    <Summary_Box>
+                        <Link to = '/Analysis_face'>
+         
+                        </Link>
+                        <Link to = '/Analysis_gaze'>
+
+                        </Link>
+                        <Link to = '/Analysis_posture'>
+
+                        </Link>
+                        <Link to = '/Analysis_speed'>
+
+                        </Link>
+                        <Link to = '/Analysis_interjection'>
+
+                        </Link>
+                    </Summary_Box>
+
+                    <Interjection_Box>
+                            워드 클라우드
+                    </Interjection_Box>
+
                 </ViewFrame2>
             </ViewFrame>
 
